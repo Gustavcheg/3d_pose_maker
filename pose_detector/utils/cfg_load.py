c410d710478408fa39dafd6d6eb396ac81a41d01
igusli03 <igusli03@gmail.com>
2024-04-25 22:26:24 +0300

    Revert "Added video detection and logging"
    This reverts commit bfbe7dcb56434a968fbb40fca7cd8745a307ba4e.

--- pose_detector/utils/cfg_load.py
+++ pose_detector/utils/cfg_load.py
@@ -1,5 +1,4 @@
 import yaml
-import loguru as lgr
 
 with open("settings.yaml", "r") as file_object:
     cfg = yaml.load(file_object, Loader=yaml.SafeLoader)
@@ -8,6 +7,5 @@ with open("settings.yaml", "r") as file_object:
 if __name__ == "__main__":
     # Change to logging
     print(cfg)
-    
     
     
--- run.py
+++ run.py
@@ -1,19 +1,7 @@
-from pose_detector.utils.tools import show_image
+from pose_detector.utils.tools import show_image, draw_landmarks_on_image
 from pose_detector.landmarks_point_detector import PoseDetector
 
-from pose_detector.utils.cfg_load import cfg
-
-from loguru import logger
-import sys
-from datetime import datetime
-
 if __name__ == "__main__":
-    logger.add(cfg['paths']['logs'] + 'logee.log', format="{time: YYYY-MM-DD HH:mm:SS} | {level} | {message}")
-    logger.info("App started!")
     poser = PoseDetector()
-    # detected_result, annotated_image = poser.detect_pose_on_image(visualize=True)
-    # show_image(annotated_image)
-
-    poser.detect_pose_on_video(visualize=True)
-
-    logger.info("App stopped!")
+    detected_result, annotated_image = poser.detect_pose_on_image()
+    show_image(annotated_image)
